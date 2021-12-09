414193bec15e91458fa11e9672ef6ecbc0c72957
Rene Zbinden <rene.zbinden@postfinance.ch>
2021-12-09 07:48:49 +0100

    fix: add jumpapp

--- run_once_packages.sh
+++ run_once_packages.sh
@@ -3,5 +3,7 @@ sudo pacman -S --noconfirm --needed noto-fonts noto-fonts-emoji
 
 sudo pacman -S --noconfirm --needed fish fisher starship
 
+yay -S aur/jumpapp --noconfirm --needed
+
 fish -c 'fisher install jethrokuan/fzf'
 fish -c 'fisher install dracula/fish'
