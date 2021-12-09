6af8d1303f7e326c116bd5cad52d40e18b32d5c1
Rene Zbinden <rene.zbinden@postfinance.ch>
2021-12-09 11:14:32 +0100

    fix: only install yay packages if necessary

--- run_once_packages.sh
+++ run_once_packages.sh
@@ -3,7 +3,14 @@ sudo pacman -S --noconfirm --needed noto-fonts noto-fonts-emoji fd
 
 sudo pacman -S --noconfirm --needed kitty fish fisher starship
 
-yay -S aur/jumpapp aur/tmux-plugin-manager --noconfirm --needed
+
+if ! type jumpapp > /dev/null; then
+  yay -S aur/jumpapp --noconfirm --needed
+fi
+
+if [! -f /usr/share/tmux-plugin-manager/tpm ]; then
+  yay -S aur/tmux-plugin-manager --noconfirm --needed
+fi
 
 fish -c 'fisher install jethrokuan/fzf'
 fish -c 'fisher install dracula/fish'
