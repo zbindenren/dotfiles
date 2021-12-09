50fa0a17e8ae5b3fe81172e65bd01098bc6ee707
Rene Zbinden <rene.zbinden@postfinance.ch>
2021-12-09 07:19:05 +0100

    feat: add fish stuff

---
+++ dot_config/private_fish/conf.d/aliases.fish
@@ -0,0 +1,21 @@
+# chrome
+alias kc="killall chromium && killall chromium && killall chromium"
+
+# systemctl
+alias start="sudo systemctl start"
+alias stop="sudo systemctl stop"
+alias stat="sudo systemctl status"
+alias restart="sudo systemctl restart"
+
+# kubectl
+alias k="kubectl"
+alias ks="kubectl sudo"
+alias ke="kubectl get events --sort-by=.metadata.creationTimestamp"
+alias kse="kubectl sudo get events --sort-by=.metadata.creationTimestamp"
+alias k9="k9s --as=zbindenren --as-group=system:masters"
+
+alias ws='cd (lab ws)'
+
+# vi
+alias vim='nvim'
+alias vi='nvim'
