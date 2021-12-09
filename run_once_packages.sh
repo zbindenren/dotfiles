c103b956088b48f5340d581a7b7596ecc6adebde
Rene Zbinden <rene.zbinden@postfinance.ch>
2021-12-09 11:11:08 +0100

    chore: update tmux

--- run_once_packages.sh
+++ run_once_packages.sh
@@ -3,7 +3,7 @@ sudo pacman -S --noconfirm --needed noto-fonts noto-fonts-emoji fd
 
 sudo pacman -S --noconfirm --needed kitty fish fisher starship
 
-yay -S aur/jumpapp --noconfirm --needed
+yay -S aur/jumpapp aur/tmux-plugin-manager --noconfirm --needed
 
 fish -c 'fisher install jethrokuan/fzf'
 fish -c 'fisher install dracula/fish'
