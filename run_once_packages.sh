4dcca48786bc45bf37387f0a4012a574b4067876
Rene Zbinden <rene.zbinden@postfinance.ch>
2021-12-09 07:20:01 +0100

    fix: add starship

--- run_once_packages.sh
+++ run_once_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo pacman -S --noconfirm --needed noto-fonts noto-fonts-emoji
 
-sudo pacman -S --noconfirm --needed fish fisher
+sudo pacman -S --noconfirm --needed fish fisher starship
 
 fish -c 'fisher install jethrokuan/fzf'
 fish -c 'fisher install dracula/fish'
