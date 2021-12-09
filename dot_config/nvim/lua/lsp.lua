ab78b748e01a538e222852a357e13185a4db939f
Rene Zbinden <rene.zbinden@postfinance.ch>
2021-12-09 06:41:52 +0100

    feat: add nvim configuration

---
+++ dot_config/nvim/lua/lsp.lua
@@ -0,0 +1,2 @@
+require'lspconfig'.bashls.setup{on_attach = require("utils").on_attach}
+
---
+++ dot_config/nvim/lua/plugins.lua
@@ -0,0 +1,209 @@
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
+
+	use("christoomey/vim-tmux-navigator")
+
+	use("ojroques/vim-oscyank")
+
+	use("simrat39/symbols-outline.nvim")
+
+	use({ "dracula/vim", as = "dracula" })
+
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
+	})
+
+	use({ "folke/which-key.nvim" })
+
+	use({ "fatih/vim-go" })
+
+	-- tpope, the legend
+	use({ "tpope/vim-commentary" })
+	use({ "tpope/vim-repeat" }) -- repeat commands
+	use({ "tpope/vim-vinegar" }) -- press - for local filebrowser
+	use({ "tpope/vim-surround" }) -- cs)] turns surrounding ) into ]
+
+	use({ "vim-test/vim-test", requires = { { "preservim/vimux" } } })
+
+	use({ "junegunn/vim-easy-align" })
+
+	-- popup markdown preview
+	use({ "npxbr/glow.nvim", run = ":GlowInstall" })
+
+	require("plenary") -- otherwise neovim crashes on startup opening a file: https://github.com/TimUntersberger/neogit/issues/206
+	use({
+		"TimUntersberger/neogit",
+		requires = { "sindrets/diffview.nvim", "nvim-lua/plenary.nvim" },
+		config = function()
+			require("neogit").setup({
+				disable_commit_confirmation = true,
+				integrations = {
+					diffview = true,
+				},
+			})
+		end,
+	})
+
+	use({
+		"lewis6991/gitsigns.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("gitsigns").setup()
+		end,
+	})
+
+	use({ "samoshkin/vim-mergetool" })
+
+	-- buffers
+	use({
+		"akinsho/nvim-bufferline.lua",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("bufferline").setup()
+		end,
+	})
+	use({ "kazhala/close-buffers.nvim" })
+
+	use({ "famiu/bufdelete.nvim" })
+
+	-- incremental syntax parsing, the mother of modernity
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+		config = function()
+			local ts = require("nvim-treesitter.configs")
+			ts.setup({ ensure_installed = "maintained", highlight = { enable = true } })
+		end,
+	})
+
+	-- trailing whitespaces
+	use({
+		"ntpeters/vim-better-whitespace",
+		config = function()
+			-- strip only if I touched the line
+			vim.g.strip_only_modified_lines = 1
+			vim.g.strip_whitespace_on_save = 1
+			vim.g.strip_whitespace_confirm = 0
+		end,
+	})
+
+	-- file tree instead of nerdtree (needs a patched font from: https://www.nerdfonts.com/)
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("nvim-tree").setup({
+				auto_close = true,
+				view = {
+					auto_resize = true,
+				},
+			})
+		end,
+	})
+
+	-- show indents
+	use({ "lukas-reineke/indent-blankline.nvim" })
+
+	use({
+		"lukas-reineke/format.nvim",
+		config = function()
+			require("format").setup({
+				lua = {
+					{
+						cmd = { "stylua" },
+					},
+				},
+				terraform = {
+					{
+						cmd = { "terraform fmt" },
+					},
+				},
+				javascript = {
+					{
+						cmd = { "prettier -w --single-quote" },
+					},
+				},
+				yaml = {
+					{
+						cmd = { "prettier -w --single-quote" },
+					},
+				},
+			})
+		end,
+	})
+
+	-- spell checking
+	use({
+		"lewis6991/spellsitter.nvim",
+		config = function()
+			require("spellsitter").setup()
+		end,
+	})
+
+	-- snippets
+	use({ "hrsh7th/vim-vsnip" })
+	use({ "hrsh7th/vim-vsnip-integ" })
+	use({ "golang/vscode-go" })
+
+	-- autocompletion
+	use({
+		"hrsh7th/nvim-compe",
+		config = function()
+			require("compe").setup({
+				enabled = true,
+				autocomplete = true,
+				debug = false,
+				min_length = 1,
+				preselect = "enable",
+				throttle_time = 80,
+				source_timeout = 200,
+				incomplete_delay = 400,
+				max_abbr_width = 100,
+				max_kind_width = 100,
+				max_menu_width = 100,
+				documentation = true,
+
+				source = { path = true, buffer = false, calc = true, nvim_lsp = true, nvim_lua = true, vsnip = true },
+			})
+		end,
+	})
+
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup()
+			require("nvim-autopairs.completion.compe").setup({
+				map_cr = true, --  map <CR> on insert mode
+				map_complete = true, -- it will auto insert `(` after select function or method item
+			})
+		end,
+	})
+
+	-- show code actions as lightbulb
+	use({ "kosayoda/nvim-lightbulb" })
+
+	use({
+		"windwp/nvim-spectre",
+		requires = { { "nvim-lua/plenary.nvim" }, { "nvim-lua/popup.nvim" } },
+	})
+
+	-- LSP goodies
+	use({
+		"onsails/lspkind-nvim",
+		"neovim/nvim-lspconfig",
+		"glepnir/lspsaga.nvim",
+		"ray-x/lsp_signature.nvim",
+	})
+
+	use({
+		"hoob3rt/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
+
+	use({ "mfussenegger/nvim-lint" })
+
+	-- incubating plugins
+end)
