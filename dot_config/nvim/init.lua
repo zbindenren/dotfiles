9950c8cadc7b52790fa241c1c63bdd1a2d2eb1e0
Rene Zbinden <rene.zbinden@postfinance.ch>
2021-12-15 08:32:30 +0100

    chore(vim): update lightbulp command

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,11 +2,11 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+	fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path })
+	execute("packadd packer.nvim")
 end
 
 local indent = 2
@@ -41,20 +41,27 @@ vim.opt.shada = { "!", "'1000", "<50", "s10", "h" } -- remember stuff across ses
 vim.api.nvim_command("set noswapfile") -- I have OCD file saving issues anyway
 
 -- restore cursor position
-vim.cmd(([[
+vim.cmd([[
   au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
-]]))
+]])
 
 -- recompile automatically when editing plugins.lua
-vim.cmd(([[
+vim.cmd([[
 autocmd BufWritePost plugins.lua PackerCompile
-]]))
+]])
 
 -- apply chezmoi on write
-vim.cmd(([[
+vim.cmd([[
 autocmd BufWritePost ~/.local/share/chezmoi/* ! chezmoi apply --source-path "%"
-]]))
+]])
 
+vim.cmd([[
+autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '+' | execute 'OSCYankReg +' | endif
+]])
+
+vim.cmd([[
+autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()
+]])
 -- set leader to space early
 vim.g.mapleader = " "
 
@@ -72,4 +79,4 @@ require("statusline")
 require("markdown")
 require("lsp")
 
-vim.cmd[[colorscheme dracula]]
+vim.cmd([[colorscheme dracula]])
