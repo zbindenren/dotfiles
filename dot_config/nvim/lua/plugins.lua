373f1fc9c286c40bc6324d52ee969bbdaf11a3b2
Rene Zbinden <rene.zbinden@postfinance.ch>
2021-12-21 08:25:47 +0100

    fix formatting

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -186,7 +186,7 @@ return require("packer").startup(function(use)
 				},
 				on_attach = function(client)
 					if client.resolved_capabilities.document_formatting then
-						vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+						vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()")
 					end
 				end,
 			})
