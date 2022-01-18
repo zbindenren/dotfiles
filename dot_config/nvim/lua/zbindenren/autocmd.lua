4aa564dcdefbb39636633e62c0bf0f8ab209c728
Rene Zbinden <rene.zbinden@postfinance.ch>
2022-01-18 07:44:23 +0100

    feat(nvim): add autocmd to write on insert leave

--- dot_config/nvim/lua/zbindenren/autocmd.lua
+++ dot_config/nvim/lua/zbindenren/autocmd.lua
@@ -1,15 +1,30 @@
 -- Autocommands
 -- restore cursor position
 vim.cmd([[
-  au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+  augroup RestoreCursorPosition
+    autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+  augroup end
 ]])
 
 -- apply chezmoi on write
 vim.cmd([[
-autocmd BufWritePost ~/.local/share/chezmoi/* ! chezmoi apply --source-path "%"
+  augroup ApplyChezmoi
+    autocmd!
+    autocmd BufWritePost ~/.local/share/chezmoi/* ! chezmoi apply --source-path "%"
+  augroup end
 ]])
 
 -- oscyank
 vim.cmd([[
-autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '+' | execute 'OSCYankReg +' | endif
+  augroup OSCYank
+    autocmd!
+    autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '+' | execute 'OSCYankReg +' | endif
+  augroup end
+]])
+
+vim.cmd([[
+  augroup AutoSaveOnInsertLeave
+    autocmd!
+    autocmd InsertLeave * ++nested write
+  augroup end
 ]])
