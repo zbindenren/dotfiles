9c775b211ac799880695b9bdea2858fe565c2235
Rene Zbinden <rene.zbinden@postfinance.ch>
2022-02-10 15:17:45 +0100

    feat(tmux): add tmK function to start k9s

--- dot_config/private_fish/conf.d/tmux.fish
+++ dot_config/private_fish/conf.d/tmux.fish
@@ -7,6 +7,13 @@ function tmn
   tmux -2 attach-session -d
 end
 
+function tmK
+  set name k9s
+  tmux new-session -s $name -d
+  tmux send-keys -t $name 'k9s --as=zbindenren --as-group=system:masters' C-m
+  tmux -2 attach-session -d
+end
+
 alias tma="tmux attach -t"
 alias tms="tmux switch -t"
 alias tml="tmux ls"
