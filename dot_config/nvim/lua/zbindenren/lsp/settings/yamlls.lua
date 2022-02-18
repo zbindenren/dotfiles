fe8c964a90dee15576bd53de50603f26b56758d8
Rene Zbinden <rene.zbinden@postfinance.ch>
2022-02-18 16:26:50 +0100

    chore: add some schema to yamlls

--- dot_config/nvim/lua/zbindenren/lsp/settings/yamlls.lua
+++ dot_config/nvim/lua/zbindenren/lsp/settings/yamlls.lua
@@ -1,7 +1,20 @@
 return {
 	settings = {
 		yaml = {
-			schemas = { kubernetes = "globPattern" },
+			validate = true,
+			schemas = {
+				["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
+				["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+				["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+				["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+				["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+				["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+				["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+				["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+				["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
+				["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose",
+				["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.23.1-standalone-strict/all.json"] = "*.yaml",
+			},
 		},
 	},
 }
