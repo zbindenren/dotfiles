c626a6cbb08d00a0816ba2b3dd99af7f24091299
Rene Zbinden <rene.zbinden@postfinance.ch>
2022-03-01 07:48:32 +0100

    fix(fish): ignore onefetch error

--- dot_config/private_fish/conf.d/chpwd.fish
+++ dot_config/private_fish/conf.d/chpwd.fish
@@ -1,7 +1,7 @@
 function chpwd --on-variable PWD --description 'handler of changing $PWD'
   if not status --is-command-substitution ; and status --is-interactive
     if test -d $PWD/.git; and type -q onefetch
-      onefetch
+      onefetch 2> /dev/null
     end
   end
 end
