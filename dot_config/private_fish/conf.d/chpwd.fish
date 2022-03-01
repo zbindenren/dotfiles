f66ccb68df593eba0a7565ea1a39f26896ab955f
Rene Zbinden <rene.zbinden@postfinance.ch>
2022-03-01 07:44:41 +0100

    fix: run onefetch only if command exists

--- dot_config/private_fish/conf.d/chpwd.fish
+++ dot_config/private_fish/conf.d/chpwd.fish
@@ -1,6 +1,6 @@
 function chpwd --on-variable PWD --description 'handler of changing $PWD'
   if not status --is-command-substitution ; and status --is-interactive
-    if test -d $PWD/.git
+    if test -d $PWD/.git; and type -q onefetch
       onefetch
     end
   end
