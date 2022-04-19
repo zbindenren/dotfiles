a8231e7f9668e7ca676d62118a703a0538d68e06
Rene Zbinden <rene.zbinden@postfinance.ch>
2022-04-19 08:15:04 +0200

    chore: update autocommands to use new api

--- dot_config/nvim/lua/zbindenren/autocmd.lua
+++ dot_config/nvim/lua/zbindenren/autocmd.lua
@@ -1,38 +1,19 @@
 -- Autocommands
 -- restore cursor position
-vim.cmd([[
-  augroup RestoreCursorPosition
-    autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
-  augroup end
-]])
+vim.api.nvim_create_autocmd("BufReadPost", {
+	command = [[if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]],
+	group = vim.api.nvim_create_augroup("RestoreCursorPosition", { clear = true }),
+})
 
 -- apply chezmoi on write
-vim.cmd([[
-  augroup ApplyChezmoi
-    autocmd!
-    autocmd BufWritePost ~/.local/share/chezmoi/* ! chezmoi apply --source-path "%"
-  augroup end
-]])
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = "./dot_config/*",
+	command = '! chezmoi apply --source-path "%"',
+	group = vim.api.nvim_create_augroup("ApplyChezmoi", { clear = false }),
+})
 
 -- highlight yanked text for 200ms using the "Visual" highlight group
-vim.cmd([[
-augroup highlight_yank
-autocmd!
-au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
-augroup END
-]])
-
--- oscyank
--- vim.cmd([[
--- augroup OSCYank
--- autocmd!
--- autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '+' | execute 'OSCYankReg +' | endif
--- augroup end
--- ]])
-
--- vim.cmd([[
---   augroup AutoSaveOnInsertLeave
---     autocmd!
---     autocmd InsertLeave * ++nested write
---   augroup end
--- ]])
+vim.api.nvim_create_autocmd("TextYankPost", {
+	command = 'silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})',
+	group = vim.api.nvim_create_augroup("HighlightYank", { clear = true }),
+})
