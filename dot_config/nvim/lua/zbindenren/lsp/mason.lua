2b520d352ea5eff2d86beff40c1f3cb3ac7f7e9b
Rene Zbinden <rene.zbinden@postfinance.ch>
2022-09-05 11:46:10 +0200

    fix: disable format for lua

--- dot_config/nvim/lua/zbindenren/lsp/mason.lua
+++ dot_config/nvim/lua/zbindenren/lsp/mason.lua
@@ -1,41 +1,43 @@
 local mason_ok, mason = pcall(require, "mason")
 if not mason_ok then
-  vim.notify("mason not found!")
-  return
+	vim.notify("mason not found!")
+	return
 end
 
 local mason_lspconfig_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_ok then
-  vim.notify("mason-lspconfig not found!")
-  return
+	vim.notify("mason-lspconfig not found!")
+	return
 end
 
 local lspconfig_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_ok then
-  vim.notify("lspconfig not found!")
-  return
+	vim.notify("lspconfig not found!")
+	return
 end
 
 local servers = {
-  "gopls",
-  "sumneko_lua",
-  "yamlls",
-  "jsonls",
+	"gopls",
+	"yamlls",
+	"jsonls",
+	"sumneko_lua",
 }
 
 mason.setup()
 mason_lspconfig.setup({
-  ensure_installed = servers,
+	ensure_installed = servers,
 })
 
+local opts = {
+	on_attach = require("zbindenren.lsp.handlers").on_attach,
+	capabilities = require("zbindenren.lsp.handlers").capabilities,
+}
+
 for _, server in ipairs(servers) do
-  local opts = {
-    on_attach = require("zbindenren.lsp.handlers").on_attach,
-    capabilities = require("zbindenren.lsp.handlers").capabilities,
-  }
+	local server_opts = require("zbindenren.lsp.settings." .. server)
+	opts = vim.tbl_deep_extend("force", server_opts, opts)
+end
 
-  local server_opts = require("zbindenren.lsp.settings." .. server)
-  opts = vim.tbl_deep_extend("force", server_opts, opts)
-
-  lspconfig[server].setup(opts)
+for _, server in ipairs(servers) do
+	lspconfig[server].setup(opts)
 end
