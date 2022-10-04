9088206309e1ac86867232ca2bdf17691c2199e7
Rene Zbinden <rene.zbinden@postfinance.ch>
2022-10-04 08:23:47 +0200

    chore: update to nvim 0.8.0

--- dot_config/nvim/lua/zbindenren/lsp/handlers.lua
+++ dot_config/nvim/lua/zbindenren/lsp/handlers.lua
@@ -46,7 +46,7 @@ end
 
 -- local function lsp_highlight_document(client)
 -- Set autocommands conditional on server_capabilities
--- if client.resolved_capabilities.document_highlight then
+-- if client.server_capabilities.document_highlight then
 -- 	vim.api.nvim_exec(
 -- 		[[
 --      augroup lsp_document_highlight
@@ -81,10 +81,10 @@ end
 M.on_attach = function(client, bufnr)
 	-- disable lsp formatting for tsserver and gopls - this is done by null-ls
 	if client.name == "tsserver" then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.document_formatting = false
 	end
 	if client.name == "sumneko_lua" then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.document_formatting = false
 	end
 	lsp_keymaps(bufnr)
 	-- lsp_highlight_document(client)
