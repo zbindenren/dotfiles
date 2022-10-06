bdfdce543e754502891c3399dde1016d81332b9c
Rene Zbinden <rene.zbinden@postfinance.ch>
2022-10-06 14:47:52 +0200

    fix: todo regexp

--- dot_config/nvim/lua/zbindenren/plugins.lua
+++ dot_config/nvim/lua/zbindenren/plugins.lua
@@ -79,14 +79,8 @@ return packer.startup(function(use)
 		requires = "nvim-lua/plenary.nvim",
 		config = function()
 			require("todo-comments").setup({
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
 				search = {
-					-- regex that will be used to match keywords.
-					-- don't replace the (KEYWORDS) placeholder
-					pattern = [[\b(KEYWORDS):(]], -- ripgrep regex
-					-- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+          pattern = "\\b(KEYWORDS)[:(]" -- ripgrep regex
 				},
 			})
 		end,
