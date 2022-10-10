c7fb65a3b8ae1d2005cb6b82bf14edf1eb3c0ec5
Rene Zbinden <rene.zbinden@postfinance.ch>
2022-10-10 08:08:57 +0200

    fix: disable command hight 0 again

--- dot_config/nvim/lua/zbindenren/options.lua
+++ dot_config/nvim/lua/zbindenren/options.lua
@@ -36,7 +36,7 @@ o.timeoutlen = 300 -- Don't wait more that 150ms for normal mode commands
 o.termguicolors = true -- True color support
 o.shada = { "!", "'1000", "<50", "s10", "h" } -- remember stuff across sessions
 o.laststatus = 3 -- use global status line
-o.cmdheight = 0 -- disable command line
+-- o.cmdheight = 1 -- disable command line
 o.laststatus = 0 -- disable last status
 
 -- folds
