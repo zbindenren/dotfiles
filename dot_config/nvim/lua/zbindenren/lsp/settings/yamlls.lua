7cc9337050968dc00b6d93b2242855ccb2c8fada
Rene Zbinden <rene.zbinden@postfinance.ch>
2023-03-29 07:49:42 +0200

    fix: tmp disable kubernetes in yamlls

--- dot_config/nvim/lua/zbindenren/lsp/settings/yamlls.lua
+++ dot_config/nvim/lua/zbindenren/lsp/settings/yamlls.lua
@@ -6,7 +6,7 @@ return {
 				url = "https://www.schemastore.org/api/json/catalog.json",
 			},
 			schemas = {
-				kubernetes = "*.yaml",
+				-- kubernetes = "*.yaml",
 				["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
 				["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
 				["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
