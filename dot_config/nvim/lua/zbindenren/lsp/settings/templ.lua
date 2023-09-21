cb34ce1fa501b81a842e07751e9375cc2b67b07d
Rene Zbinden <rene.zbinden@postfinance.ch>
2023-09-21 07:25:31 +0200

    chore: add templ formatter

---
+++ dot_config/nvim/lua/zbindenren/lsp/settings/templ.lua
@@ -0,0 +1,10 @@
+return {
+	settings = {
+		templ = {
+			cmd = { "templ", "lsp" },
+			filetypes = { "templ" },
+			-- root_dir = nvim_lsp.util.root_pattern("go.mod", ".git"),
+			settings = {},
+		},
+	},
+}
