e65ef217588d3e3cbd4d1409b1029a0a209181cb
Rene Zbinden <rene.zbinden@postfinance.ch>
2024-01-18 07:19:46 +0100

    chore: add atuin

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
   # Commands to run in interactive sessions can go here
   bind \cF forward-bigword
   bind -k nul accept-autosuggestion
+
+  atuin init fish | source
 end
 
 set -x PATH $PATH ~/go/bin ~/bin
