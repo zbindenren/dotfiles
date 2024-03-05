6378a7ab8ebe3304a8af31f4917b81fe80c73a92
Rene Zbinden <rene.zbinden@postfinance.ch>
2024-03-05 19:43:26 +0100

    fix: add flash

---
+++ lua/plugins/ui/flash.lua
@@ -0,0 +1,19 @@
+local status_ok, flash = pcall(require, 'flash')
+if not status_ok then
+  return
+end
+
+local icons = require('lib.icons')
+
+flash.setup({
+  prompt = {
+    enabled = true,
+    prefix = { { icons.ui.Separator .. icons.ui.Rocket .. icons.ui.ChevronRight .. ' ', 'FlashPromptIcon' } },
+    win_config = { relative = 'editor', width = 1, height = 1, row = 1, col = 0, zindex = 1000 },
+  },
+  modes = {
+    char = {
+      jump_labels = false,
+    },
+  },
+})
