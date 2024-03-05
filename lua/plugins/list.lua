ac2d44feb70746112e467cf577238c55e2d76479
Rene Zbinden <rene.zbinden@postfinance.ch>
2024-03-05 08:19:45 +0100

    chore: update

--- lua/plugins/list.lua
+++ lua/plugins/list.lua
@@ -38,6 +38,18 @@ local plugins = {
     cmd = 'NvimTreeToggle',
   },
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    cmd = 'Neotree',
+    config = load_config('ui.neo-tree'),
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  },
+  {
     'folke/which-key.nvim',
     config = load_config('ui.which-key'),
     event = 'VeryLazy',
