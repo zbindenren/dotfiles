101bf352697e615775b9e1a2c3bd0bc60e8bda54
Rene Zbinden <rene.zbinden@postfinance.ch>
2024-03-14 15:28:44 +0100

    fix: add snippets

--- lua/plugins/ui/cmp.lua
+++ lua/plugins/ui/cmp.lua
@@ -8,7 +8,9 @@ if not snip_status_ok then
   return
 end
 
-require('luasnip/loaders/from_vscode').lazy_load()
+require('luasnip/loaders/from_vscode').lazy_load({
+  paths = vim.fn.stdpath("config") .. "/snippets",
+})
 
 local compare = require('cmp.config.compare')
 
