2595efdbe10a3eb6f8b5cd1b316249bcbf4c8c30
Rene Zbinden <rene.zbinden@postfinance.ch>
2024-05-21 06:44:11 +0200

    chore: update neogit conf

--- dot_config/nvim/lua/plugins/tools/neogit.lua
+++ dot_config/nvim/lua/plugins/tools/neogit.lua
@@ -5,84 +5,53 @@ if not ngit_status_ok then
 end
 
 ngit.setup {
-  -- Hides the hints at the top of the status buffer
+  use_default_keymaps = true,
   disable_hint = false,
-  -- Disables changing the buffer highlights based on where the cursor is.
   disable_context_highlighting = false,
-  -- Disables signs for sections/items/hunks
   disable_signs = false,
-  -- Changes what mode the Commit Editor starts in. `true` will leave nvim in normal mode, `false` will change nvim to
-  -- insert mode, and `"auto"` will change nvim to insert mode IF the commit message is empty, otherwise leaving it in
-  -- normal mode.
-  disable_insert_on_commit = "auto",
-  -- When enabled, will watch the `.git/` directory for changes and refresh the status buffer in response to filesystem
-  -- events.
+  graph_style = "ascii",
   filewatcher = {
-    interval = 1000,
     enabled = true,
   },
-  -- "ascii"   is the graph the git CLI generates
-  -- "unicode" is the graph like https://github.com/rbong/vim-flog
-  graph_style = "ascii",
-  -- Used to generate URL's for branch popup action "pull request".
+  telescope_sorter = function()
+    return nil
+  end,
   git_services = {
     ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
     ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
     ["gitlab.com"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
-    ["gitlab.pnet.ch"] = "https://gitlab.pnet.ch/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
   },
-  -- Allows a different telescope sorter. Defaults to 'fuzzy_with_index_bias'. The example below will use the native fzf
-  -- sorter instead. By default, this function returns `nil`.
-  telescope_sorter = function()
-    return require("telescope").extensions.fzf.native_fzf_sorter()
-  end,
-  -- Persist the values of switches/options within and across sessions
-  remember_settings = true,
-  -- Scope persisted settings on a per-project basis
-  use_per_project_settings = true,
-  -- Table of settings to never persist. Uses format "Filetype--cli-value"
-  ignored_settings = {
-    "NeogitPushPopup--force-with-lease",
-    "NeogitPushPopup--force",
-    "NeogitPullPopup--rebase",
-    "NeogitCommitPopup--allow-empty",
-    "NeogitRevertPopup--no-edit",
-  },
-  -- Configure highlight group features
   highlight = {
     italic = true,
     bold = true,
-    underline = true
+    underline = true,
   },
-  -- Set to false if you want to be responsible for creating _ALL_ keymappings
-  use_default_keymaps = true,
-  -- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
-  -- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
+  disable_insert_on_commit = "auto",
+  use_per_project_settings = true,
+  show_head_commit_hash = true,
+  remember_settings = true,
+  fetch_after_checkout = false,
   auto_refresh = true,
-  -- Value used for `--sort` option for `git branch` command
-  -- By default, branches will be sorted by commit date descending
-  -- Flag description: https://git-scm.com/docs/git-branch#Documentation/git-branch.txt---sortltkeygt
-  -- Sorting keys: https://git-scm.com/docs/git-for-each-ref#_options
   sort_branches = "-committerdate",
-  -- Change the default way of opening neogit
   kind = "tab",
-  -- Disable line numbers and relative line numbers
   disable_line_numbers = true,
   -- The time after which an output console is shown for slow running commands
-  console_timeout = 5000,
+  console_timeout = 7000,
   -- Automatically show console if a command takes more than console_timeout milliseconds
   auto_show_console = true,
+  notification_icon = "󰊢",
   status = {
     recent_commit_count = 10,
   },
   commit_editor = {
-    kind = "auto",
+    kind = "tab",
   },
   commit_select_view = {
     kind = "tab",
   },
   commit_view = {
     kind = "vsplit",
+    verify_commit = vim.fn.executable("gpg") == 1,
   },
   log_view = {
     kind = "tab",
@@ -96,6 +65,9 @@ ngit.setup {
   merge_editor = {
     kind = "auto",
   },
+  description_editor = {
+    kind = "auto",
+  },
   tag_editor = {
     kind = "auto",
   },
@@ -105,31 +77,25 @@ ngit.setup {
   popup = {
     kind = "split",
   },
+  refs_view = {
+    kind = "tab",
+  },
   signs = {
-    -- { CLOSED, OPENED }
     hunk = { "", "" },
     item = { ">", "v" },
     section = { ">", "v" },
   },
-  -- Each Integration is auto-detected through plugin presence, however, it can be disabled by setting to `false`
   integrations = {
-    -- If enabled, use telescope for menu selection rather than vim.ui.select.
-    -- Allows multi-select and some things that vim.ui.select doesn't.
     telescope = nil,
-    -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
-    -- The diffview integration enables the diff popup.
-    --
-    -- Requires you to have `sindrets/diffview.nvim` installed.
-    diffview = true,
-
-    -- If enabled, uses fzf-lua for menu selection. If the telescope integration
-    -- is also selected then telescope is used instead
-    -- Requires you to have `ibhagwan/fzf-lua` installed.
+    diffview = nil,
     fzf_lua = nil,
   },
   sections = {
-    -- Reverting/Cherry Picking
     sequencer = {
+      folded = false,
+      hidden = false,
+    },
+    bisect = {
       folded = false,
       hidden = false,
     },
@@ -174,84 +140,11 @@ ngit.setup {
       hidden = false,
     },
   },
-  mappings = {
-    commit_editor = {
-      ["q"] = "Close",
-      ["<c-c><c-c>"] = "Submit",
-      ["<c-c><c-k>"] = "Abort",
-    },
-    rebase_editor = {
-      ["p"] = "Pick",
-      ["r"] = "Reword",
-      ["e"] = "Edit",
-      ["s"] = "Squash",
-      ["f"] = "Fixup",
-      ["x"] = "Execute",
-      ["d"] = "Drop",
-      ["b"] = "Break",
-      ["q"] = "Close",
-      ["<cr>"] = "OpenCommit",
-      ["gk"] = "MoveUp",
-      ["gj"] = "MoveDown",
-      ["<c-c><c-c>"] = "Submit",
-      ["<c-c><c-k>"] = "Abort",
-    },
-    finder = {
-      ["<cr>"] = "Select",
-      ["<c-c>"] = "Close",
-      ["<esc>"] = "Close",
-      ["<c-n>"] = "Next",
-      ["<c-p>"] = "Previous",
-      ["<down>"] = "Next",
-      ["<up>"] = "Previous",
-      ["<tab>"] = "MultiselectToggleNext",
-      ["<s-tab>"] = "MultiselectTogglePrevious",
-      ["<c-j>"] = "NOP",
-    },
-    -- Setting any of these to `false` will disable the mapping.
-    popup = {
-      ["?"] = "HelpPopup",
-      ["A"] = "CherryPickPopup",
-      ["D"] = "DiffPopup",
-      ["M"] = "RemotePopup",
-      ["P"] = "PushPopup",
-      ["X"] = "ResetPopup",
-      ["Z"] = "StashPopup",
-      ["b"] = "BranchPopup",
-      ["c"] = "CommitPopup",
-      ["f"] = "FetchPopup",
-      ["l"] = "LogPopup",
-      ["m"] = "MergePopup",
-      ["p"] = "PullPopup",
-      ["r"] = "RebasePopup",
-      ["v"] = "RevertPopup",
-      ["w"] = "WorktreePopup",
-    },
-    status = {
-      ["q"] = "Close",
-      ["I"] = "InitRepo",
-      ["1"] = "Depth1",
-      ["2"] = "Depth2",
-      ["3"] = "Depth3",
-      ["4"] = "Depth4",
-      ["<tab>"] = "Toggle",
-      ["x"] = "Discard",
-      ["s"] = "Stage",
-      ["S"] = "StageUnstaged",
-      ["<c-s>"] = "StageAll",
-      ["u"] = "Unstage",
-      ["U"] = "UnstageStaged",
-      ["$"] = "CommandHistory",
-      ["#"] = "Console",
-      ["Y"] = "YankSelected",
-      ["<c-r>"] = "RefreshBuffer",
-      ["<enter>"] = "GoToFile",
-      ["<c-v>"] = "VSplitOpen",
-      ["<c-x>"] = "SplitOpen",
-      ["<c-t>"] = "TabOpen",
-      ["{"] = "GoToPreviousHunkHeader",
-      ["}"] = "GoToNextHunkHeader",
-    },
+  ignored_settings = {
+    "NeogitPushPopup--force-with-lease",
+    "NeogitPushPopup--force",
+    "NeogitPullPopup--rebase",
+    "NeogitCommitPopup--allow-empty",
   },
 }
 
