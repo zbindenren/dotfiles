15a10225f5183cd2dbbe144d7569b31a7775a6b0
Rene Zbinden <rene.zbinden@postfinance.ch>
2024-10-04 09:55:08 +0200

    fix: autoimport go packages

--- dot_config/nvim/lua/core/autocmd.lua
+++ dot_config/nvim/lua/core/autocmd.lua
@@ -120,25 +120,29 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 })
 
 
--- Go organize imports on save.
--- vim.api.nvim_create_autocmd("BufWritePre", {
---   pattern = { "*.go" },
---   callback = function()
---     local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
---     params.context = { only = { "source.organizeImports" } }
---
---     local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
---     for _, res in pairs(result or {}) do
---       for _, r in pairs(res.result or {}) do
---         if r.edit then
---           vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
---         else
---           vim.lsp.buf.execute_command(r.command)
---         end
---       end
---     end
---   end
--- })
+-- Autoimport go packages.
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.go" },
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { "source.organizeImports" } }
+
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    for _, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local client = vim.lsp.get_client_by_id(vim.lsp.get_clients()[1].id)
+          if client then
+            local enc = client.offset_encoding or "utf-16"
+            vim.lsp.util.apply_workspace_edit(r.edit, enc)
+          end
+        elseif r.command then
+          vim.lsp.buf.execute_command(r.command)
+        end
+      end
+    end
+  end
+})
 
 -- Lint when writing file.
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
