128f4ca9498df54d297a5bba0602a37f026b9ad1
Rene Zbinden <rene.zbinden@postfinance.ch>
2024-10-11 06:40:43 +0200

    chore(fish): add task -a alias

--- dot_config/private_fish/conf.d/functions.fish
+++ dot_config/private_fish/conf.d/functions.fish
@@ -3,3 +3,9 @@ function task_select
   set task (echo $line |awk  '{print $2}'| sed 's/:$//')
   go-task $task
 end
+
+function task_select_all
+  set line (task -a | grep -v Available| fzf) # Clean up the output
+  set task (echo $line |awk  '{print $2}'| sed 's/:$//')
+  go-task $task
+end
