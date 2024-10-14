da8201562e2c64da1e156358c742cd1ece37d496
Rene Zbinden <rene.zbinden@postfinance.ch>
2024-10-14 13:11:45 +0200

    feat(fish): new task alias

--- dot_config/private_fish/conf.d/functions.fish
+++ dot_config/private_fish/conf.d/functions.fish
@@ -9,3 +9,9 @@ function task_select_all
   set task (echo $line |awk  '{print $2}'| sed 's/:$//')
   go-task $task
 end
+
+function task_select_global
+  set line (task -g -l | grep -v Available| fzf) # Clean up the output
+  set task (echo $line |awk  '{print $2}'| sed 's/:$//')
+  go-task $task
+end
