352d9da76e373c8e10555e85e33b921e4a818c15
Rene Zbinden <rene.zbinden@postfinance.ch>
2024-11-05 17:11:28 +0100

    feat(nvim): add new usercommand AddCommentHeader

--- dot_config/nvim/lua/core/usercmd.lua
+++ dot_config/nvim/lua/core/usercmd.lua
@@ -1,3 +1,4 @@
+--------------------------------------------------------------------------------
 -- Reload neovim config
 vim.api.nvim_create_user_command("ReloadConfig", function()
   for name, _ in pairs(package.loaded) do
@@ -10,6 +11,7 @@ vim.api.nvim_create_user_command("ReloadConfig", function()
   vim.notify("Nvim configuration reloaded!", vim.log.levels.INFO)
 end, {})
 
+--------------------------------------------------------------------------------
 -- Copy relative path
 vim.api.nvim_create_user_command("CRpath", function()
   local path = vim.fn.expand("%")
@@ -17,6 +19,7 @@ vim.api.nvim_create_user_command("CRpath", function()
   vim.notify('Copied "' .. path .. '" to the clipboard!')
 end, {})
 
+--------------------------------------------------------------------------------
 -- Copy absolute path
 vim.api.nvim_create_user_command("CApath", function()
   local path = vim.fn.expand("%:p")
@@ -24,6 +27,7 @@ vim.api.nvim_create_user_command("CApath", function()
   vim.notify('Copied "' .. path .. '" to the clipboard!')
 end, {})
 
+--------------------------------------------------------------------------------
 -- Toggle line numbers.
 vim.api.nvim_create_user_command("ToggleNum", function()
   vim.cmd("Gitsigns toggle_signs")
@@ -36,6 +40,7 @@ vim.api.nvim_create_user_command("ToggleNum", function()
   end
 end, {})
 
+--------------------------------------------------------------------------------
 -- Toggle list chars.
 vim.api.nvim_create_user_command("ToggleList", function()
   if vim.o.list then
@@ -45,6 +50,7 @@ vim.api.nvim_create_user_command("ToggleList", function()
   end
 end, {})
 
+--------------------------------------------------------------------------------
 -- Toggle clipboard.
 vim.api.nvim_create_user_command("ToggleClipboard", function()
   if vim.o.clipboard == "unnamedplus" then
@@ -56,6 +62,7 @@ vim.api.nvim_create_user_command("ToggleClipboard", function()
   end
 end, {})
 
+--------------------------------------------------------------------------------
 -- Toggle Diagnostic virtual text.
 vim.g.diagnostic_virtual_text = true
 
@@ -64,11 +71,13 @@ vim.api.nvim_create_user_command("ToggleDiagnosticVirtualText", function()
   vim.diagnostic.config({ virtual_text = vim.g.diagnostic_virtual_text })
 end, {})
 
+--------------------------------------------------------------------------------
 -- Chat with Anhropic
 vim.api.nvim_create_user_command('CodeCompanionChatAnthropic', function()
   vim.cmd('CodeCompanionChat anthropic')
 end, {})
 
+--------------------------------------------------------------------------------
 -- Toggle format on save
 local function toggle_format_on_save()
   vim.g.format_on_save_enabled = not vim.g.format_on_save_enabled
@@ -82,7 +91,8 @@ end
 vim.api.nvim_create_user_command('ToggleFormatOnSave', toggle_format_on_save, {})
 
 
--- Remove item in quckfix list
+--------------------------------------------------------------------------------
+-- Remove item in quickfix list
 function Remove_qf_item()
   local curqfidx = vim.fn.line('.')
   local qfall = vim.fn.getqflist()
@@ -107,3 +117,64 @@ end
 
 vim.cmd("command! RemoveQFItem lua Remove_qf_item()")
 vim.api.nvim_command("autocmd FileType qf nnoremap <buffer> dd :RemoveQFItem<cr>")
+
+
+--------------------------------------------------------------------------------
+-- AddCommentHeader
+local function get_comment_string()
+  local node = vim.treesitter.get_node()
+  if not node then return '//' end
+
+  local lang = vim.treesitter.language.get_lang(vim.bo.filetype) or 'go'
+  local parser = vim.treesitter.get_parser(0, lang)
+  local tstree = parser:parse()[1]
+  local root = tstree:root()
+
+  local query = vim.treesitter.query.parse(lang, [[
+        (comment) @comment
+    ]])
+
+  for _, n in query:iter_captures(root, 0) do
+    local text = vim.treesitter.get_node_text(n, 0)
+    local comment_start = text:match("^%s*([^%s]+)")
+    if comment_start then
+      return comment_start
+    end
+  end
+
+  return '//' -- Default to '//' if no comment is found
+end
+
+
+local function create_comment_line(comment_string)
+  local max_length = 80
+  local comment_length = #comment_string
+  local available_space = max_length - comment_length
+  local sep_length = math.max(1, available_space)
+
+  -- Ensure the separator fills up to the 80th character
+  local separator = string.rep("-", sep_length)
+  local full_comment = comment_string .. separator
+
+  -- If the full comment is shorter than 80 characters, extend the separator
+  if #full_comment < max_length then
+    separator = separator .. string.rep("-", max_length - #full_comment)
+    full_comment = comment_string .. separator
+  end
+
+  return full_comment
+end
+
+local function add_comment_header()
+  local comment_string = get_comment_string()
+  local line = vim.api.nvim_get_current_line()
+
+  local header_line = create_comment_line(comment_string)
+  local content_line = comment_string .. " " .. line
+
+  local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+  vim.api.nvim_buf_set_lines(0, row - 1, row, false, { header_line, content_line })
+  vim.api.nvim_win_set_cursor(0, { row + 1, 0 })
+end
+
+vim.api.nvim_create_user_command("AddCommentHeader", add_comment_header, {})
