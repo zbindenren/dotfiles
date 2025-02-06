b15e3f578dfa2701e17f4b99993ccfd10c4661c2
Rene Zbinden <rene.zbinden@postfinance.ch>
2025-02-06 09:10:06 +0100

    feat(fish): add yazi function

--- dot_config/private_fish/conf.d/functions.fish
+++ dot_config/private_fish/conf.d/functions.fish
@@ -37,3 +37,13 @@ function tma
       tmux attach -t $session
     end
 end
+
+# yazi integration
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
