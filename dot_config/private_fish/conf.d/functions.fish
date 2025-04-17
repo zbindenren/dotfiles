f552d7647e5d05ddc07aa19fb2233e0dd1916e05
Rene Zbinden <rene.zbinden@postfinance.ch>
2025-04-17 16:02:16 +0200

    fix: task funcs

--- dot_config/private_fish/conf.d/functions.fish
+++ dot_config/private_fish/conf.d/functions.fish
@@ -1,19 +1,19 @@
 function task_select
     set line (task -l | grep -v Available| fzf) # Clean up the output
     set task (echo $line |awk  '{print $2}'| sed 's/:$//')
-    go-task $task
+    task $task
 end
 
 function task_select_all
     set line (task -a | grep -v Available| fzf) # Clean up the output
     set task (echo $line |awk  '{print $2}'| sed 's/:$//')
-    go-task $task
+    task $task
 end
 
 function task_select_global
     set line (task -g -l | grep -v Available| fzf) # Clean up the output
     set task (echo $line |awk  '{print $2}'| sed 's/:$//')
-    go-task -g $task
+    task -g $task
 end
 
 # kill tmux sessions
