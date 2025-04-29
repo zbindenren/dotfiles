fb5c700d4e6dd86fff2b854e49837d5d1445158a
Rene Zbinden <rene.zbinden@postfinance.ch>
2025-04-29 06:38:20 +0200

    fix(nvim): add non conflicting key to toggle hidden files in snacks

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -3,6 +3,15 @@ return {
   opts = {
     indent = {
       enabled = false,
-    }
-  }
+    },
+    picker = {
+      win = {
+        input = {
+          keys = {
+            ["<a-a>"] = { "toggle_hidden", mode = { "i", "n" } },
+          },
+        },
+      },
+    },
+  },
 }
