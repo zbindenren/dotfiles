96c3cf28723b03107db6a2d70e2a7f31290195f2
Rene Zbinden <rene.zbinden@postfinance.ch>
2025-05-04 16:17:16 +0200

    fix(tmux): make tmn func more resilient to errors

--- dot_config/private_fish/conf.d/tmux.fish
+++ dot_config/private_fish/conf.d/tmux.fish
@@ -1,5 +1,19 @@
 function tmn
+    if set -q TMUX
+        echo "Error: cannot create new session within tmux session" >&2
+        return 1
+    end
+
     set name (basename $PWD)
+    if test -n "$argv[1]"
+        set name "$argv[1]"
+    end
+
+    if tmux has-session -t $name 2>/dev/null
+        echo "Error: tmux session '$name' already exists" >&2
+        return 1
+    end
+
     tmux new-session -s $name -d -n neovim
     tmux send-keys -t $name:neovim vi C-m
     tmux new-window -t $name -n term01
