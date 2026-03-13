51169372beb626e3fe744397568a8109488fc099
Rene Zbinden <rene.zbinden@postfinance.ch>
2026-03-13 13:59:03 +0100

    chore(fish): remove unused CREATE_GO_APP_IGNORE env var

--- dot_config/private_fish/conf.d/env.fish
+++ dot_config/private_fish/conf.d/env.fish
@@ -12,7 +12,4 @@ set -gx LESS -X
 
 set -gx TASK_X_REMOTE_TASKFILES 1
 
-# create-go-app
-set -gx CREATE_GO_APP_IGNORE Makefile
-
 set -gx NODE_EXTRA_CA_CERTS /opt/homebrew/etc/ca-certificates/cert.pem
